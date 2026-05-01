e56ac08ea34df85dd859ecaafbc1aec924dadd40
haticeyalcinn <htcyalcin38@gmail.com>
2026-05-01 15:12:59 +0300

    colors dosyası oluşturuldu

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF1E1E1E);
+  static const Color buttonDark = Color(0xFF2D2D2D);
+  static const Color scientific = Color(0xFF4A7A8A);
+  static const Color orange = Color(0xFFFF9500);
+  static const Color red = Color(0xFFE53935);
+  static const Color green = Color(0xFF43A047);
+  static const Color pink = Color(0xFFE91E63);
+}
